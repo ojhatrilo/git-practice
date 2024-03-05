865431cb5f13e118d395733b495f8ca05d2f3d84
Ojhatrilo <ojhatrilochan5@gmail.com>
2024-03-05 14:46:20 +0530

    Updated

--- src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
+++ src/main/java/rahulshettyacademy/AbstractComponents/AbstractComponent.java
@@ -14,6 +14,7 @@ import rahulshettyacademy.pageobjects.CartPage;
 import rahulshettyacademy.pageobjects.OrderPage;
 
 public class AbstractComponent {
+	// a small update to run git hub
 	
 	WebDriver driver;
 
